2062c5da01bfff52df1d6af2001020676dbe3d9e
Kevin Casey <kevincasey@Kevins-MBP.lan>
2023-08-13 21:25:25 -0400

    update index and css

--- docs/search.js
+++ docs/search.js
@@ -1,21 +1,34 @@
-let supportedSymbols = [];
+function searchStocks(query) {
+  // If the query is empty, hide the autocomplete suggestions
+  if (query === '') {
+    document.getElementById('autocomplete').innerHTML = '';
+    return;
+  }
 
-// Fetch the supported symbols on page load
-fetch(`https://cloud.iexapis.com/stable/ref-data/symbols?token=${IEX_TOKEN}`)
-    .then(response => response.json())
-    .then(data => {
-        supportedSymbols = data;
+  // Construct the API URL with the query and token
+  const url = `https://cloud.iexapis.com/stable/ref-data/symbols?token=${IEX_TOKEN}`;
+
+  // Fetch the data from the API
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Filter the data based on the query and create a list of suggestions
+      let suggestions = data.filter(stock => stock.symbol.startsWith(query.toUpperCase()));
+      
+      // Limit the number of suggestions if needed
+      suggestions = suggestions.slice(0, 10);
+
+      // Create the HTML for the suggestions
+      let html = '<ul>';
+      suggestions.forEach(stock => {
+        html += `<li>${stock.symbol} - ${stock.name}</li>`;
+      });
+      html += '</ul>';
+
+      // Insert the suggestions into the autocomplete div
+      document.getElementById('autocomplete').innerHTML = html;
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
     });
-
-document.getElementById('searchButton').addEventListener('click', () => {
-    const query = document.getElementById('search').value;
-
-    // Find the symbol that matches the query
-    const matchingSymbol = supportedSymbols.find(symbol => symbol.symbol === query.toUpperCase());
-
-    if (matchingSymbol) {
-        window.location.href = `/chart.html?symbol=${matchingSymbol.symbol}`;
-    } else {
-        alert('Symbol not found');
-    }
-});
+}
