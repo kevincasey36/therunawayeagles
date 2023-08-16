2b4063b5596e2a6142333fdfce15254ab916b0ca
Kevin Casey <kevincasey@Kevins-MBP.lan>
2023-08-15 21:54:57 -0400

    update search.js and index.html

--- docs/search.js
+++ docs/search.js
@@ -1,34 +1,33 @@
+function handleSearch() {
+    const query = document.getElementById('search').value;
+    searchStocks(query);
+}
+
 function searchStocks(query) {
-  // If the query is empty, hide the autocomplete suggestions
-  if (query === '') {
-    document.getElementById('autocomplete').innerHTML = '';
-    return;
-  }
+    // If the query is empty, hide the autocomplete suggestions
+    if (query === '') {
+        document.getElementById('autocomplete').innerHTML = '';
+        return;
+    }
 
-   // Construct the URL to point to your Flask backend
-  const url = `/search/${query}`;
-  
-  // Fetch the data from the API
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      // Filter the data based on the query and create a list of suggestions
-      let suggestions = data.filter(stock => stock.symbol.startsWith(query.toUpperCase()));
-      
-      // Limit the number of suggestions if needed
-      suggestions = suggestions.slice(0, 10);
+    // Construct the API URL for your Flask backend
+    const url = `/search/${query}`;
 
-      // Create the HTML for the suggestions
-      let html = '<ul>';
-      suggestions.forEach(stock => {
-        html += `<li>${stock.symbol} - ${stock.name}</li>`;
-      });
-      html += '</ul>';
+    // Fetch the data from the API
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            // Create the HTML for the suggestions based on the returned data
+            let html = '<ul>';
+            data.forEach(stock => {
+                html += `<li>${stock.symbol} - ${stock.name}</li>`;
+            });
+            html += '</ul>';
 
-      // Insert the suggestions into the autocomplete div
-      document.getElementById('autocomplete').innerHTML = html;
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+            // Insert the suggestions into the autocomplete div
+            document.getElementById('autocomplete').innerHTML = html;
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
 }
